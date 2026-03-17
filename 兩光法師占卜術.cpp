83b2a305946be1ba5c508ca8c076b08194744682
s1130176-ship-it <s1130176@dmhs.kh.edu.tw>
2026-03-17 11:35:59 +0800

    解題過程

---
+++ 兩光法師占卜術.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int M, D, S;
+	cin >> M >> D;
+	S = ( M*2+D ) % 3;
+	if (s == 2) {
+		cout << "大吉" << endl
+	}else if (s == 1) {
+		cout << "吉"<< endl;
+	}else {
+		cout << "普通" << endl;	
+	}
+	return 0;
+}
