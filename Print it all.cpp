67c88eac1dda05cdb01added8e6c48ee3148f11b
s1130176-ship-it <s1130176@dmhs.kh.edu.tw>
2026-04-14 11:33:28 +0800

    Create Print it all.cpp

---
+++ Print it all.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	while (cin >> n) {
+		for (int i = 1; i < n; i++) {
+			if (i % 7 != 0) {
+				cout << i << " ";
+			}
+		}
+		cout << "\n";
+	}
+	return 0;
